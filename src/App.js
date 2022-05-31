efea37169edaefaf85849f2095ab64a0bcaeba95
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-31 16:12:02 +0200

    integrate Formik

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import Page from './pages/Page';
+import PrivacyPage from './pages/Privacy';
+import SignupPage from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        {/*"static" pages*/}
         <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/privacy" element={<PrivacyPage/>}></Route>
+
+        {/*Auth pages*/}
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/signup" element={<SignupPage/>}></Route>
+
+        {/*User Pages*/}
+        <Route path="/p/:username" element={<Page/>}></Route>
+        <Route path="/p/:username/edit" element={<Page/>}></Route>
       </Routes>
     </div>
   );
