0131717603a67cf2f65dd62a89b026d3221ea359
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-06-01 14:04:32 +0200

    add boilerplate sass code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Page from './pages/Page';
