9ca0c0e8a70f9f1acbe57e2d2a174c15c2090f10
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-06-01 22:04:02 +0200

    link navbar to actual links and data

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,23 +1,36 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { selectUserPage, selectUserProfile } from "../../store/user/selectors";
 import "./style.scss";
 export default function NavBar() {
-  //Only shows up if user is logged in
-  //TODO: build an enclosure menu
+  const user = useSelector(selectUserProfile);
+  const page = useSelector(selectUserPage);
   return (
     <nav>
       <ul>
         <li>
-          <p>Username</p>
+          <p>{user.name}</p>
         </li>
         <li>
           <details className="menu">
             <summary>Menu</summary>
             <div className="menu-inner">
               <ul>
-                <li>Profile Settings</li>
-                <li>Edit Links</li>
-                <li>Edit Sections</li>
-                <li>Logout</li>
+                <li>
+                  <Link to={`/p/${user.username}`}>My Homepage</Link>
+                </li>
+                <li>
+                  <Link to="/">Profile Settings</Link>
+                </li>
+                <li>
+                  <Link to="/">Edit Links</Link>
+                </li>
+                <li>
+                  <Link to="/">Edit Sections</Link>
+                </li>
+                <li>
+                  <Link to="/">Log Out</Link>
+                </li>
               </ul>
             </div>
           </details>
