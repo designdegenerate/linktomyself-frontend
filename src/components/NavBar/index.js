e775728664c9863f22529e1b580beba9942cd6d9
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-06-01 15:51:34 +0200

    add navbar menu, fix color styles

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,26 @@
 import { Link } from 'react-router-dom';
 import './style.scss';
 export default function NavBar() {
+  //Only shows up if user is logged in
+  //TODO: build an enclosure menu
   return (
     <nav>
-      <p>Hello</p>
+      <ul>
+        <li>
+          <p>Username</p>
+        </li>
+        <li>
+          <details>
+            <summary>Menu</summary>
+            <ul className='menu'>
+              <li>Profile/User Settings</li>
+              <li>Edit Links</li>
+              <li>Edit Sections</li>
+              <li>Logout</li>
+            </ul>
+          </details>
+        </li>
+      </ul>
     </nav>
   )
 }
