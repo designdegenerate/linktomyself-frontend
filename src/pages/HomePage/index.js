ffc4623a4885bdda08127361f7c68f5600e7fe73
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-06-29 21:29:45 +0200

    show link to example page

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -16,16 +16,35 @@ export default function HomePage() {
       <article>
         <div className="header">
           <picture>
-            <source srcSet={homeImageDark} media="(prefers-color-scheme: dark)" type="image/png"></source>
+            <source
+              srcSet={homeImageDark}
+              media="(prefers-color-scheme: dark)"
+              type="image/png"
+            ></source>
             <img src={homeImage} alt="" type="image/png"></img>
           </picture>
-          <h1>Your uncomplicated personal homepage in a <br></br>decentralized world</h1>
-          <Link to="/signup" className="button-border">
-            Create yours
-          </Link>
+          <h1>
+            Your uncomplicated personal homepage in a <br></br>decentralized
+            world
+          </h1>
+          <div>
+            <Link to="/p/laurensspangenberg" className="button-border" target="_blank">
+              View Mine
+            </Link>
+            <Link to="/signup" className="button-filled">
+              Create yours
+            </Link>
+          </div>
         </div>
         <div className="subtitle">
-          <p><strong>Because a personal homepage is a public profile</strong>. Most website builders are focused on structure with ugly, complicated, and bloated defaults. We keep it simple: tell a bit about yourself, show off your links to relevant places, show a selection of favourite things with an optional list to a full collection, and choose a colour scheme (with dark mode support). What more do you really need?
+          <p>
+            <strong>Because a personal homepage is a public profile</strong>.
+            Most website builders are focused on structure with ugly,
+            complicated, and bloated defaults. We keep it simple: tell a bit
+            about yourself, show off your links to relevant places, show a
+            selection of favourite things with an optional list to a full
+            collection, and choose a colour scheme (with dark mode support).
+            What more do you really need?
           </p>
         </div>
       </article>
